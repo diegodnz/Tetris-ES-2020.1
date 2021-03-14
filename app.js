0579767e7d6eb86f73cfcdb4233534c1705c8e77
Alisson Diego <diegodiniz991@gmail.com>
2021-03-14 00:58:04 -0300

    Criando primeira peça e colorindo os grids

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 document.addEventListener('DOMContentLoaded'), () => {
-    const width = 10
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
     const score = document.querySelector('#score')
     const startBt = document.querySelector('#start-button')
+
+    const width = 9
+
+    const iShape = [
+        [0, width, width*2, width*3],  // |
+        [0, -1, 1, 2],  // -
+        [width, width-1, width+1, width+2], // -
+        [0, width, width*2, width*3]  // |
+    ]
+
+    function drawGrids() {
+        squares.forEach(div => div.classList.add("noPiece"))
+    }
+
+    function testPiece() {
+        squares[4].classList.add("piece")
+    }
+
+    drawGrids();
+    testPiece();
 } 
