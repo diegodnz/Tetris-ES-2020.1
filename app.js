0ac4a7fa723cf9105051757522aaed85d952a644
Alisson Diego <diegodiniz991@gmail.com>
2021-03-14 00:51:05 -0300

    Query selectors

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded'), () => {
-
+    const width = 10
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const score = document.querySelector('#score')
+    const startBt = document.querySelector('#start-button')
 } 
