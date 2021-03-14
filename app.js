2b30dbc5e93cf8cefd9a48379c5311fea8704bab
Alisson Diego <diegodiniz991@gmail.com>
2021-03-14 01:09:07 -0300

    Criando todas as peças

--- app.js
+++ app.js
@@ -7,14 +7,46 @@ document.addEventListener('DOMContentLoaded'), () => {
     const width = 9
 
     const iShape = [
-        [0, width, width*2, width*3],  // |
-        [0, -1, 1, 2],  // -
-        [width, width-1, width+1, width+2], // -
-        [0, width, width*2, width*3]  // |
+        [0, width, width*2, width*3], 
+        [0, -1, 1, 2],  
+        [width, width-1, width+1, width+2], 
+        [0, width, width*2, width*3] 
     ]
+
+    const zShape = [
+        [0, +1, -width, width+1],
+        [0, +1, width, width-1],
+        [0, +1, -width, width+1],
+        [0, +1, width, width-1]
+    ]
+
+    const lShape = [
+        [0, -width, width, width+1],
+        [0, 1, -1, width-1],
+        [0, width, -width, -width-1],
+        [0, -1, +1, -width+1]
+    ]
+
+    const strangeShape = [
+        [0, -width, -1, 1],
+        [0, -width, width, 1],
+        [0, 1, -1, width],
+        [0, -width, width, -1]
+    ]
+
+    let currentPos = 13
+    let currentPiece = strangeShape[3]
 
     function drawGrids() {
         squares.forEach(div => div.classList.add("noPiece"))
+    }
+
+    function drawPiece() {
+        for (index of currentPiece) {
+            if(currentPos+index > 0){
+                squares[currentPos + index].classList.add("piece")
+            }
+        }        
     }
 
     function testPiece() {
@@ -22,5 +54,5 @@ document.addEventListener('DOMContentLoaded'), () => {
     }
 
     drawGrids();
-    testPiece();
+    drawPiece();
 } 
