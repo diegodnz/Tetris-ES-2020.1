8301299ee1be9dd185e49bd3038f7497f788ddb2
Alisson Diego <diegodiniz991@gmail.com>
2021-03-14 01:41:56 -0300

    Adicionando key events

--- app.js
+++ app.js
@@ -4,30 +4,30 @@ document.addEventListener('DOMContentLoaded'), () => {
     const score = document.querySelector('#score')
     const startBt = document.querySelector('#start-button')
 
-    const height = 10
+    const height = 10  // Número que representa a posição logo abaixo de uma quadrado no grid (quadrado + height = quadrado abaixo)
 
-    const iShape = [
+    const iShape = [  // Peça em forma de I
         [0, height, height*2, height*3], 
         [0, -1, 1, 2],  
         [height, height-1, height+1, height+2], 
         [0, height, height*2, height*3] 
     ]
 
-    const zShape = [
+    const zShape = [  // Peça em forma de Z
         [0, +1, -height, height+1],
         [0, +1, height, height-1],
         [0, +1, -height, height+1],
         [0, +1, height, height-1]
     ]
 
-    const lShape = [
+    const lShape = [  // Peça em forma de L
         [0, -height, height, height+1],
         [0, 1, -1, height-1],
         [0, height, -height, -height-1],
         [0, -1, +1, -height+1]
     ]
 
-    const strangeShape = [
+    const strangeShape = [  // Quarta peça
         [0, -height, -1, 1],
         [0, -height, height, 1],
         [0, 1, -1, height],
@@ -37,19 +37,28 @@ document.addEventListener('DOMContentLoaded'), () => {
     let pieces = [iShape, zShape, lShape, strangeShape]
     let currentPos = 4
     let currentPiece = nextPiece()
-    timer = setInterval(moveDown, 60)
+    timer = setInterval(moveDown, 700)
 
+    /*
+    Retorna uma peça aleatória
+    */
     function nextPiece() {
         i = Math.floor(Math.random() * pieces.length)
         return pieces[i][0]
     }
 
+    /*
+    Desenha todo o grid (marca os quadrados como "noPiece")
+    */
     function drawGrids() {
         for (let i=0; i<squares.length; i++) {           
             squares[i].classList.add("noPiece")            
         }  
     }
 
+    /*
+    Apaga uma peça
+    */
     function undrawPiece() {
         for (index of currentPiece) {
             if (currentPos+index > 0) {
@@ -58,6 +67,9 @@ document.addEventListener('DOMContentLoaded'), () => {
         }
     }
 
+    /*
+    Desenha uma peça
+    */
     function drawPiece() {
         for (index of currentPiece) {
             if(currentPos+index > 0){
@@ -66,17 +78,30 @@ document.addEventListener('DOMContentLoaded'), () => {
         }        
     }
 
+    /*
+    Cria uma nova peça na parte de cima do grid
+    */
+    function updatePiece() {
+        currentPos = 4
+        currentPiece = nextPiece()
+    }
+
+    /*
+    Para a peça caso atenda às condições
+    */
     function stop() {
         for (index of currentPiece) {
             if (index+currentPos+height > 199 || squares[index+currentPos+height].classList.contains("freeze")) {   
                 currentPiece.forEach(index => squares[currentPos+index].classList.add("freeze"))           
-                currentPos = 4
-                currentPiece = nextPiece()
+                updatePiece()
                 break
             }                       
         }
     }
 
+    /*
+    Move a peça atual para baixo
+    */
     function moveDown() {  
         stop()             
         undrawPiece()
@@ -84,7 +109,57 @@ document.addEventListener('DOMContentLoaded'), () => {
         drawPiece()    
     }
 
-    nextPiece()    
+    /*
+    Move a peça atual para a esquerda
+    */
+    function moveLeft() {
+        let atLimit = false
+        let pieceOnLeft = false
+        for (index of currentPiece) {
+            if ((currentPos+index) % height === 0) {
+                atLimit = true
+                break
+            } else if (squares[currentPos+index-1].classList.contains("freeze")) {
+                pieceOnLeft = true
+                break
+            }
+        }
+        if (!atLimit && !pieceOnLeft) {
+            undrawPiece()
+            currentPos -= 1
+            drawPiece()
+        } else if(pieceOnLeft){
+            updatePiece()
+        }
+    }
+
+    /*
+    Move a peça de acordo com a tecla pressionada
+    */
+    function control(e) {
+        if (e.keyCode === 37) {
+            moveLeft()
+        } else if (e.keyCode === 38) {
+            rotate()
+        } else if (e.keyCode === 39) {
+            moveRight()            
+        } else if (e.keyCode === 40) {
+            moveDown()
+        }
+    }
+
+    /*
+    Move a peça para baixo mais rapidamente
+    */
+    function putFast(e) {
+        if (e.keyCode === 40) {
+            moveDown()
+        }
+    }
+
+    document.addEventListener('keydown', control)
+    document.addEventListener('keydown', putFast)
+  
     drawGrids()
     drawPiece() 
 } 
